7898e85aade577e18438557ae40872a5f159bf58
bayubahe7 <liliane@Lilianes-MacBook-Air.local>
2024-05-16 10:44:59 +0200

    codewars challenge solutions

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -56,3 +56,45 @@ const divisor = 2;
 const arrayx = [1,2,3,4,5,6]; 
 
 console.log(arrayx.filter((value) => value % divisor === 0));
+
+/*
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+*/
+function countSheeps(sheep) {
+  let count = 0;
+   for (let i=0; i < sheep.length; i++){
+    if (sheep[i] === true) {
+      count++;
+    } else {
+      count;
+    }
+   }
+   return count;
+ }
+ //Write a function which calculates the average of the numbers in a given list.
+
+//Note: Empty arrays should return 0.
+
+//Solution
+function findAverage(array) {
+  let sum = 0;
+  if (array.length === 0) {
+    return 0; //for the note condition above
+  }
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum/array.length;
+}
