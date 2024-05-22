f56e88c50af92b95b81c97186732da9142def046
bayubahe7 <liliane@Lilianes-MacBook-Air.local>
2024-05-22 12:55:12 +0200

    html and css with pirple

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -349,6 +349,38 @@ const numbers = [3, 5, 1, 8, -2, 7];
 console.log(min(numbers)); // Output: -2
 console.log(max(numbers)); // Output: 8
 
+/*
+DESCRIPTION:
+Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+Examples
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7
+*/
+
+//my solution
+const stringToNumber = function(str){
+  let result = 0;
+  if(typeof str === 'string'){
+   result = Number(str);
+  }
+  return result;
+}
+
+//other variation
+
+/*
+var stringToNumber = function(str){
+  return parseInt(str, 10);
+}
+*/
 
 
 
