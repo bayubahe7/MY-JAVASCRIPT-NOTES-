131dbacc509477de364112dba2871eb23a2e5390
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-03 17:18:03 +0200

    8kyu sum negative countpositives

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -559,7 +559,7 @@ Examples
 * Input: [2,2,1,2,1], output = [2,2,2,1]
 
 */
-//solution
+//solution 7kyu
 
 function removeSmallest(numbers) {
   if (numbers.length === 0) {
@@ -571,6 +571,20 @@ function removeSmallest(numbers) {
   return copy;
 }
 //to review it later
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+*/
+
+
+
 
 
 
