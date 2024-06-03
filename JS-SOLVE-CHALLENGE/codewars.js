4568e057e9877ee21fd3ec7535cdfac928bbef54
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-03 17:36:34 +0200

    8kyu sum of negative count postives answered

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -582,7 +582,24 @@ Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 */
+// assisted answer
 
+function countPositivesSumNegatives(input) {
+  if (!input || input.length === 0){ //checks if array is empty
+    return[];
+  }
+  let countPositives = 0;
+  let sumNegatives = 0;
+  for (i = 0; i < input.length; i++){
+    if(input[i] > 0){
+      
+    countPositives ++;
+    } else if(input[i] < 0){
+      sumNegatives += input[i];
+    } 
+  }
+  return[countPositives,sumNegatives];
+}
 
 
 
