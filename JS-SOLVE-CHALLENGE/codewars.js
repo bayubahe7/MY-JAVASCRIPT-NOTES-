761cd7cbb9a89af75c7010a50363ff95db7b6443
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-05 08:42:25 +0200

    8kyu:Remove exclamation marks

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -639,6 +639,24 @@ function greet (name, owner) {
     return message;
 }
 
+//8kyu:Remove exclamation marks
+
+/*
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+//my answer
+function removeExclamationMarks(s) {
+  let myArr = s.split('');
+  let newArray = myArr.filter((value) => {
+    return value !== '!';
+  });
+  return newArray.join('');
+}
+//alterinative answers using global flag and reduce()
+function removeExclamationMarks(s) {
+  return s.replace(/!/gi, '');
+}
+
 
 
 
