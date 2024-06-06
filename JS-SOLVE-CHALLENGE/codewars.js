0b6a59f8347a8ee72f257904646b0b11cca3b84d
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-06 11:29:31 +0200

    8 kyu:If you can't sleep, just count sheepgit add .

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -804,6 +804,25 @@ function oddOrEven(array) {
     return "odd";
   }
 }
+//8 kyu:If you can't sleep, just count sheep!!
+
+/*
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+*/
+
+//my answer
+var countSheep = function (num){
+  //your code here
+  let message = "";
+  for(let i=1; i <= num; i++){
+      message += `${i} sheep...`;
+    }
+    return message;
+}
 
 
 
