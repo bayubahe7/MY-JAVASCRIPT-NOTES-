4190b91e677d2041cceac329f4058c62b133f57b
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-06 10:49:57 +0200

    7 kyu :Odd or Even?

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -747,6 +747,46 @@ function mixedArrNumberAndStr (myArray){
 }
 console.log(mixedArrNumberAndStr([1,'5','2','4',3,6])); //Output: -1;
 
+//7 kyu :Odd or Even?
+
+/*
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+Have fun!
+*/
+
+//my answer
+function oddOrEven(array) {
+ let sum = 0;
+ array.forEach((value) =>{
+   if(array.length === 0){
+     value = 0;
+     sum += 0;
+   }else if(Number.isInteger(value) === true){
+     sum += value;
+   }
+   return sum;
+ });
+ if(sum %2 === 0){
+   return 'even';
+ } else {
+   return 'odd';
+ }
+}
 
 
 
