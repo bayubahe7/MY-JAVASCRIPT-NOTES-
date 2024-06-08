cb7687f5f7bd57e522b32dd404f8a4c8ffa43cdf
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-08 20:39:28 +0200

    8 kyu:Abbreviate a Two Word Name

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -990,6 +990,12 @@ function abbrevName(name) {
   return startingChar.join('.');
 }
 
+//other methods
+
+function abbrevName(name){
+  return name.split(' ').map(x => x.substr(0, 1).toUpperCase()).join('.');
+}
+
 
 
 
