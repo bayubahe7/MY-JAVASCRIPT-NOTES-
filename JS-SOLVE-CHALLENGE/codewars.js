fb28d3297d9487d6013ccd9426016fa0f1c0e013
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-09 16:38:34 +0200

    8kyu removes elements

--- JS-SOLVE-CHALLENGE/codewars.js
+++ JS-SOLVE-CHALLENGE/codewars.js
@@ -1014,6 +1014,29 @@ function doubleInteger(i) {
 
 const doubleInteger = i  => i*2;
 
+//8 kyu:Removing Elements
+
+/*
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+//my revised answer
+
+function removeEveryOther(arr){
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
+    if (i % 2 === 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
 
 
 
