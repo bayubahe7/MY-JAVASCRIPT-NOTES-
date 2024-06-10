958b4918ed8acd911f3c96c13c4df97b1bb88bad
bayubahe7 <mukunzichadrac@gmail.com>
2024-06-10 10:16:49 +0200

    switch statement explained

--- loops.js
+++ loops.js
@@ -41,3 +41,59 @@
       }//here it will loop though the array and log all values of an array
 
       //conclusion for in is for objects and for of is for an array
+
+      //The JavaScript Switch Statement
+      //Use the switch statement to select one of many code blocks to be executed.
+
+      //Syntax
+ switch(expression) {
+          case x:
+            // code block
+            break;
+          case y:
+            // code block
+            break;
+          default:
+            // code block
+        }
+ //This is how it works:
+ /*
+ The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+If there is no match, the default code block is executed.
+*/
+
+//example
+
+/*
+The getDay() method returns the weekday as a number between 0 and 6.
+
+(Sunday=0, Monday=1, Tuesday=2 ..)
+
+This example uses the weekday number to calculate the weekday name:
+*/
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+//The result of day will be: agiven day you are on
