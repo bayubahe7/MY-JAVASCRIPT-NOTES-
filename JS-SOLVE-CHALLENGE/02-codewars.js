176476abbb772cdcc16d8b64e1bdf1b3145ce3fa
bayubahe7 <mukunzichadrac@gmail.com>
2024-08-01 10:57:05 +0200

    codewars in action

--- JS-SOLVE-CHALLENGE/02-codewars.js
+++ JS-SOLVE-CHALLENGE/02-codewars.js
@@ -24,3 +24,22 @@ const setAlarm = (employed, vacation) => employed && !vacation;//arrow function 
 function setAlarm(employed, vacation){
     return (employed && !vacation) ? true : false; // not operator and ternary operator
   }
+  /*
+  Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+*/
+
+//my answer
+function doubleChar(str) {
+    // Your code here
+    let result = "";
+    for (let i = 0; i < str.length; i++){
+      result += str[i] + str[i]
+    }
+    return result;
+  }
+  
