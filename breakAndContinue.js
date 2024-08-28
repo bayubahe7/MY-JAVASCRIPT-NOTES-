6e0e554b254d3d82200a14a767bf76f9170562d8
bayubahe7 <mukunzichadrac@gmail.com>
2024-08-28 14:00:39 +0200

    break and continue with labelname

--- breakAndContinue.js
+++ breakAndContinue.js
@@ -12,7 +12,7 @@ for (let i = 0; i < 10; i++) {
 
 //In the example above, the break statement ends the loop ("breaks" the loop) when the loop counter (i) is 3.
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*
 The Continue Statement
@@ -26,4 +26,37 @@ for (let i = 0; i < 10; i++) {
     if (i === 3) { continue; }
     text += "The number is " + i + "<br>";
   }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+JavaScript Labels
+To label JavaScript statements you precede the statements with a label name and a colon:
+
+label:
+statements
+The break and the continue statements are the only JavaScript statements that can "jump out of" a code block.
+
+Syntax:
+
+break labelname;
+
+continue labelname;
+The continue statement (with or without a label reference) can only be used to skip one loop iteration.
+
+The break statement, without a label reference, can only be used to jump out of a loop or a switch.
+
+With a label reference, the break statement can be used to jump out of any code block:
+
+Example
+*/
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+list: {
+  text += cars[0] + "<br>";
+  text += cars[1] + "<br>";
+  break list;
+  text += cars[2] + "<br>";
+  text += cars[3] + "<br>";
+}
+//A code block is a block of code between { and }.
+
 
